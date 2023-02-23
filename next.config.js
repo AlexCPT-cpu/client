90fb2152aa8bf733c28115d68f4d4ec61752d640
AlexCPT-cpu <67722561+AlexCPT-cpu@users.noreply.github.com>
2023-02-23 14:45:19 +0100

    Created a new build with all rarity

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  image: {
+    domains: ['https://gateway.ipfs.io/'],
+},
+}
+
+module.exports = nextConfig
